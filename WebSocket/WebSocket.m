3ccd07b70edbc688bfd1128be88f9cf6b3989e4b
Markus Piipari <markus@bitfluid.com>
2014-04-01 20:54:25 +0300

    Fixed /* comment mishap

--- WebSocket/WebSocket.m
+++ WebSocket/WebSocket.m
@@ -56,7 +56,7 @@
 #define HUNGRY_SIZE 5
 
 /* unused
-/*static inline BOOL WS_OP_IS_FINAL_FRAGMENT(UInt8 frame) {
+static inline BOOL WS_OP_IS_FINAL_FRAGMENT(UInt8 frame) {
 	return (frame & 0x80) ? YES : NO;
 }*/
 
